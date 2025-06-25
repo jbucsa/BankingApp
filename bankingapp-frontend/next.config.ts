d1c432a8a1fbb0eb8e40103bb54de26af60354ab
jbucsa <justin.bucsa@gmail.com>
2025-06-24 23:46:28 -0500

    Edit the next.config.ts file for vercel.

--- bankingapp-frontend/next.config.ts
+++ bankingapp-frontend/next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
+const path = require('path');
+
 const nextConfig: NextConfig = {
   /* config options here */
+};
+
+module.exports = {
+  webpack: (config: { resolve: { alias: { [x: string]: any; }; }; }) => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  }
 };
 
 export default nextConfig;
